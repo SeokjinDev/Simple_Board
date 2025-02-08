d21d8ad670b8a16295476d9fb5a2ae4d37a0389c
SeokjinDev <tjrwls1108011@gmail.com>
2025-02-08 23:48:44 +0900

    [FEAT] Add Swagger UI

--- src/main/java/com/practice/simple_board/api/HealthCheckController.java
+++ src/main/java/com/practice/simple_board/api/HealthCheckController.java
@@ -1,10 +1,17 @@
 package com.practice.simple_board.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Health Check", description = "서버 상태 확인 API 입니다.")
 @RestController
 public class HealthCheckController {
+    @Operation(
+            summary = "Health Check API",
+            description = "서버 응답을 확인하는 API 입니다."
+    )
     @GetMapping("ping")
     public String ping() {
         return "pong";
